0a160b8a90c827133bf2b802c937ecd6ca317351
c0nsol3 <consol3@protonmail.com>
2022-07-14 14:05:28 +0200

    properly activate env

--- payout.sh
+++ payout.sh
@@ -2,7 +2,7 @@
 cd ~/core3-tbw/
 
 # activate py env
-source .venv/tbw/bin/activate
+source ~/.venv/tbw/bin/activate
 
 # move unpaid into staging table which is then used to start a payout
 python core/tbw.py --manualPay
